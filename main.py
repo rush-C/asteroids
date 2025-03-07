68440ed7f1a19ddc09ea3fc06abe40fe6bf75dd4
Fakhri Joe Isgandarov <64754867+rush-C@users.noreply.github.com>
2025-03-07 11:16:04 -0500

    Few modifications and circleshape.py

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))   # TO GET A NEW GUI WINDOW
-
+    clock = pygame.time.Clock()
+    dt = 0            
     game_loop = True
     while game_loop:
         for event in pygame.event.get():
@@ -16,9 +13,11 @@ def main():
                 return
 
         screen.fill(000)
-
-
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+        
 
 
 
